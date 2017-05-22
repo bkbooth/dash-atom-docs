04fa07a528a275353defdffc09c6da65f70eb9c6
Ben Booth <bkbooth@gmail.com>
2017-05-22 21:50:17 +1000

    Add local versions of octicons font files

--- modules/config.js
+++ modules/config.js
@@ -25,6 +25,10 @@ module.exports = {
   // Static files to be copied into docset
   staticFiles: [
     { name: 'styles-dash.css', dest: path.join(basePath, 'Contents', 'Resources', 'Documents', 'assets', 'styles-dash.css') },
+    { name: 'octicons.eot', dest: path.join(basePath, 'Contents', 'Resources', 'Documents', 'assets', 'octicons.eot') },
+    { name: 'octicons.woff', dest: path.join(basePath, 'Contents', 'Resources', 'Documents', 'assets', 'octicons.woff') },
+    { name: 'octicons.ttf', dest: path.join(basePath, 'Contents', 'Resources', 'Documents', 'assets', 'octicons.ttf') },
+    { name: 'octicons.svg', dest: path.join(basePath, 'Contents', 'Resources', 'Documents', 'assets', 'octicons.svg') },
     { name: 'Info.plist', dest: path.join(basePath, 'Contents', 'Info.plist') },
     { name: 'icon.png', dest: path.join(basePath, 'icon.png') },
   ],
