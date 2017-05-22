0d9b9b5247d2e5dc568073a06b20c459ea6c9ec9
Ben Booth <ben.booth@blake.com.au>
2017-05-22 15:10:24 +1000

    Copy and transform all files into docset

---
+++ modules/build-docs.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2), { alias: { 'atom-version': 'a' } });
+const mkdirp = require('mkdirp');
+const cheerio = require('cheerio');
+const config = require('./config');
+
+config.atomVersion = argv['atom-version'] || process.env.npm_package_atom_version;
+if (!config.atomVersion) {
+  console.error(
+    'Atom version not provided.\n',
+    'Provide as --atom-version (or -a) command-line argument or set atom_version in package.json'
+  );
+  process.exit(1);
+}
+
+console.log(`Processing Atom docs v${config.atomVersion}`);
+
+mkdirp.sync(path.join(config.docsDir, 'assets'));
+copyDocsCss(config);
+copyStaticFiles(config);
+transformDocsHtml(config);
+
+
+// Find, process and copy all docs HTML into docset
+function transformDocsHtml(config) {
+  const docsFilesDir = path.join(config.scrapeDir, 'atom.io', 'docs', 'api', `v${config.atomVersion}`);
+  const docsFilesList = fs.readdirSync(docsFilesDir);
+
+  docsFilesList.forEach((docsFileName) => {
+    const fileData = fs.readFileSync(path.join(docsFilesDir, docsFileName));
+    const $ = cheerio.load(fileData, {
+      normalizeWhitespace: true,
+    });
+
+    // Exit if page is 404
+    if (/404/.test($('head title').text())) {
+      console.log(`Ignoring ${docsFileName}...`);
+      return;
+    }
+
+    console.log(`Processing ${docsFileName}...`);
+
+    // Remove unwanted elements
+    config.selectorsToRemove.forEach((selectorToRemove) => $(selectorToRemove).remove());
+
+    // Add a new header block
+    $.root().prepend(`
+      <head>
+        <meta charset="utf-8">
+        <title>${docsFileName}</title>
+        <link rel="stylesheet" href="assets/${config.atomCssFilename}">
+        <link rel="stylesheet" href="assets/${config.staticFiles[0].name}">
+      </head>
+    `);
+
+    // Write the transformed file
+    fs.writeFileSync(path.join(config.docsDir, docsFileName), $.html());
+  });
+}
+
+// Find and copy the Atom docs CSS into docset
+function copyDocsCss(config) {
+  const docsCssRegex = /^application-[0-9a-zA-Z]+\.css$/;
+  const docsAssetsDir = path.join(config.scrapeDir, 'github-atom-io-herokuapp-com.global.ssl.fastly.net', 'assets');
+  const docsAssetsFiles = fs.readdirSync(docsAssetsDir);
+  const docsCssFile = docsAssetsFiles.find((docsAssetsFile) => docsCssRegex.test(docsAssetsFile));
+  if (!docsCssFile) {
+    console.error('Failed finding Atom docss CSS to copy into docset');
+    process.exit(1);
+  }
+
+  console.log(`Copying Atom docs CSS file '${docsCssFile}' to '${config.atomCssFilename}'...`);
+
+  const readStream = fs.createReadStream(path.join(docsAssetsDir, docsCssFile));
+  const writeStream = fs.createWriteStream(path.join(config.docsDir, 'assets', config.atomCssFilename));
+  readStream.once('error', handleFailedCssCopy);
+  writeStream.once('error', handleFailedCssCopy);
+  readStream.pipe(writeStream);
+
+  function handleFailedCssCopy(err) {
+    console.error('Failed copying Atom docs CSS file.', err.message);
+    process.exit(1);
+  }
+}
+
+// Copy static files in place
+function copyStaticFiles(config) {
+  config.staticFiles.forEach((staticFile) => {
+    console.log(`Copying static file '${staticFile.name}' into docset...`);
+
+    const readStream = fs.createReadStream(path.join('assets', staticFile.name));
+    const writeStream = fs.createWriteStream(staticFile.dest);
+    readStream.once('error', handleFailedFileCopy);
+    writeStream.once('error', handleFailedFileCopy);
+    readStream.pipe(writeStream);
+  });
+
+  function handleFailedFileCopy(err) {
+    console.error('Failed copying file.', err.message);
+    process.exit(1);
+  }
+}
--- modules/config.js
+++ modules/config.js
@@ -1,15 +1,30 @@
 const path = require('path');
-const basePath = path.join('', 'Atom.docset');
+const basePath = path.join('.', 'Atom.docset');
 
 module.exports = {
+  // Location of the docset database
   dbFile: path.join(basePath, 'Contents', 'Resources', 'docSet.dsidx'),
+
+  // Location of the docset documents
   docsDir: path.join(basePath, 'Contents', 'Resources', 'Documents'),
-  listIndexes: [
-    { type: 'Class', fileName: 'class_list.html' },
-    { type: 'Method', fileName: 'method_list.html' },
-    { type: 'File', fileName: 'file_list.html' },
+  // Location of the scraped documents
+  scrapeDir: path.join('.', 'scraped'),
+  // CSS selectors to find and remove from HTML documents
+  selectorsToRemove: [
+    'head',
+    'meta',
+    '.top-bar',
+    '.documents > .wrapper > form',
+    '.column.sidebar',
+    '.footer-pad',
+    'footer',
   ],
+  // Name of Atom stylesheet
+  atomCssFilename: 'styles-atom.css',
+
+  // Static files to be copied into docset
   staticFiles: [
+    { name: 'styles-dash.css', dest: path.join(basePath, 'Contents', 'Resources', 'Documents', 'assets', 'styles-dash.css') },
     { name: 'Info.plist', dest: path.join(basePath, 'Contents', 'Info.plist') },
     { name: 'icon.png', dest: path.join(basePath, 'icon.png') },
   ],
